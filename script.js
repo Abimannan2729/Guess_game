a90684127e692a626db9b30474f168440ef6103a
Abimannan2729 <abimannan2729@outlook.com>
2024-10-10 06:28:06 +0530

    Update

---
+++ script.js
@@ -0,0 +1,30 @@
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 10;
+ console.log (randomNumber);
+function checkGuess() {
+  const inputElement = document.getElementById("guess");
+  const feedbackElement = document.getElementById("feedback");
+  const guess = Number(inputElement.value); // Convert input to a number
+  attempts--;
+
+  if (guess === randomNumber) {
+    feedbackElement.innerText = "Congratulations!";
+    feedbackElement.style.color = "green";
+    attempts = 0; // End the game
+  } else if (guess < randomNumber) {
+    feedbackElement.innerText = `Too low! Try again. ${attempts} attempts remaining.`;
+    feedbackElement.style.color = "red";
+  } else {
+    feedbackElement.innerText = `Too high! Try again. ${attempts} attempts remaining.`;
+    feedbackElement.style.color = "red";
+  }
+
+  if (attempts === 0) {
+    feedbackElement.innerText = `Game over. ${randomNumber} is correct.`;
+    feedbackElement.style.color = "red";
+   document.getElementById("but").disabled= true;
+   document.getElementById("but").style.color="gray";
+  }
+
+  inputElement.value = ""; // Clear input after guess
+}
